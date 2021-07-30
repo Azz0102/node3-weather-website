d619106b4241d6f7a3c76d0d0c59e3b2e88ca1ed
Azz0102 <76864866+Azz0102@users.noreply.github.com>
2021-07-30 21:42:44 +0700

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'Huy'
+messageOne.textContent = ''
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
