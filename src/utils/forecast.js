cb6278239d8643040acb8af2c1e137109c6e5d09
Azz0102 <76864866+Azz0102@users.noreply.github.com>
2021-07-30 22:03:03 +0700

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,9 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to connect to weather service!', undefined)
         } else if (response.body.error) {
             callback('Unable to find location. Please try again', undefined)
-        } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+        } else { 
+            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} 
+            degrees out. It feels like ${response.body.current.feelslike} degrees out. The humidity is ${response.body.current.humidity}%.`)
         }
     })
 }
